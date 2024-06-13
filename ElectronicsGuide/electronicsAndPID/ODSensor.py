c51407702b3ac9a674f2999e5235c104d8222672
s225100 <s225100@student.dtu.dk>
2024-06-13 13:46:31 +0200

    OD sensor

--- ElectronicsGuide/electronicsAndPID/ODSensor.py
+++ ElectronicsGuide/electronicsAndPID/ODSensor.py
@@ -6,14 +6,19 @@ from machine import ADC, Pin
 
 class ODSensor:
     
-    
-    def __init__(self, pinNumber):
-        self.adc = ADC(Pin(pinNumber))
+    def __init__(self):
+        optimal_DAC = 232
+        pin = machine.Pin(25)
+
+        self.dac = DAC(pin, bits=8)
+        self.dac.write(optimal_DAC)
+
+        self.adc = ADC(pin)
         self.adc.atten(ADC.ATTN_11DB)
         
     def getOD(self):
         return self.adc.read()
-    
-    
+
+
     
 
