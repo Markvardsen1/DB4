c418bfd96749cb52fa4817bc25060af32a3832fb
Mathias Markvardsen <129381407+Markvardsen@users.noreply.github.com>
2024-06-16 09:44:27 +0200

    started adding unit test for smallDCMotor and stepperMotor

--- DB4 - inside board/Systems/electronicsAndPID/StepperMotor.py
+++ DB4 - inside board/Systems/electronicsAndPID/StepperMotor.py
@@ -10,6 +10,9 @@ class StepperMotor:
         DEFAULT_frequency = 0
         DEFAULT_duty_cycle = 0
         
+        minCycles = 450 #TODO TEST THIS VALUE
+        maxCycles = 1023 #TODO TEST THIS VALUE
+        
         
         def __init__(self, step_pin_number: int, dir_pin_number : int):
                 self.step_pin = machine.Pin(step_pin_number, machine.Pin.OUT)
@@ -35,9 +38,17 @@ class StepperMotor:
                         time.sleep(self.delay)
                         self.stop()
 
-        def setDutyCycle(self, dutyCycle : int):
+        def setSpeedCycle(self, dutyCycle : int):
                 self.duty_cycle = dutyCycle
                 self.pwm.duty(dutyCycle)
+
+        def setSpeedPercentage(self, percentage : int):
+                self.duty_cycle = self.percentageToDutyCycle(percentage)
+                self.pwm.duty(self.duty_cycle)
+
+        def getSpeedPercentage(self):
+                percentage = self.dutyCycleToPercentage(self.duty_cycle)
+                return percentage
         
         def setFreq(self, frequency : int ):
                 self.frequency = frequency
@@ -64,3 +75,13 @@ class StepperMotor:
                         self.pwm.freq(freq)
                         time.sleep(delay)
                         print(freq)
+                        
+
+        def percentageToDutyCycle(self, percentage):
+                dutyCycle = ((self.maxCycles - self.minCycles)*percentage/100) + self.minCycles
+                return dutyCycle
+
+        
+        def dutyCycleToPercentage(self, dutyCycle):
+                percentage = 100 * ((dutyCycle - self.minCycles)/(self.maxCycles - self.minCycles))
+                return percentage
