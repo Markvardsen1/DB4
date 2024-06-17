1ffb706ecd3dcaca0fc2644b6215e8b035989b5e
Mathias Markvardsen <129381407+Markvardsen@users.noreply.github.com>
2024-06-17 13:56:37 +0200

    adding tests for stepperMotor

--- DB4 - inside board/tests/stepperMotor90degreesTest.py
+++ DB4 - inside board/tests/stepperMotor90degreesTest.py
@@ -1,19 +1,5 @@
 import time 
 import machine
-from Systems.Software.StepperClass import Stepper
+from Systems import components
 
-stepperMotor = Stepper(33, 27, None, 400, 10, False, -1)
-
-def turn90degreesAndBack() -> None:
-    stepperMotor.target_deg(90)
-    time.sleep(1)
-    stepperMotor.target_deg(0)
-    time.sleep(1)
-
-def turning90degreesTest() -> None:
-    turn90degreesAndBack()
-    stepperMotor.stop()
-
-if __name__ == '__main__':
-    turning90degreesTest()
-    
+components.stepperMotor.turn90Degrees()
